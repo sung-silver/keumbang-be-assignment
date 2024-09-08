e20ff58f5aa4edef36637a094228f1987f9ccf74
sung-silver <jinna0319@gmail.com>
2024-09-08 21:25:43 +0900

    [FEAT] auth: Member 엔티티 구현

---
+++ keumbang-auth/src/main/java/com/keumbang/auth/domain/Member.java
@@ -0,0 +1,47 @@
+package com.keumbang.auth.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+import com.keumbang.auth.domain.enums.MemberRole;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long memberId;
+
+  @Enumerated(EnumType.STRING)
+  @NotNull
+  private MemberRole role;
+
+  @NotNull private String username;
+
+  @NotNull private String email;
+
+  @NotNull private String password;
+
+  private String refreshToken;
+
+  @Builder
+  private Member(
+      MemberRole role, String username, String email, String password, String refreshToken) {
+    this.role = role;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.refreshToken = refreshToken;
+  }
+}
