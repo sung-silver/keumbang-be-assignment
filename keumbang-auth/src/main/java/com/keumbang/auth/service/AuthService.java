3db7ff1a0f46370ff8d11691fa01ae856b51406e
sung-silver <jinna0319@gmail.com>
2024-09-09 00:13:19 +0900

    [FEAT] auth: AuthService에서 signUp 구현

---
+++ keumbang-auth/src/main/java/com/keumbang/auth/service/AuthService.java
@@ -0,0 +1,41 @@
+package com.keumbang.auth.service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.keumbang.auth.common.jwt.JwtTokenService;
+import com.keumbang.auth.controller.dto.request.SignUpRequest;
+import com.keumbang.auth.controller.dto.response.GetTokenResponse;
+import com.keumbang.auth.domain.Member;
+import com.keumbang.auth.repository.MemberRepository;
+import com.keumbang.auth.service.vo.MemberAuthVO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+  private final MemberRepository memberRepository;
+  private final JwtTokenService jwtTokenService;
+
+  @Transactional
+  public GetTokenResponse signUp(final SignUpRequest request) {
+    final Member member =
+        Member.builder()
+            .username(request.username())
+            .email(request.email())
+            .password(request.password())
+            .build();
+
+    memberRepository.save(member);
+
+    List<String> roles = new ArrayList<>(Arrays.asList(member.getRole().getRole()));
+    MemberAuthVO memberAuthVO = MemberAuthVO.of(member.getMemberId(), roles);
+
+    return jwtTokenService.issueToken(memberAuthVO);
+  }
+}
