fde70362607984a2fc0377bf210d9606bc3bca33
sung-silver <jinna0319@gmail.com>
2024-09-08 22:26:48 +0900

    [FEAT] auth: GetTokenResponse에 of 연산자 구현

--- keumbang-auth/src/main/java/com/keumbang/auth/controller/dto/response/GetTokenResponse.java
+++ keumbang-auth/src/main/java/com/keumbang/auth/controller/dto/response/GetTokenResponse.java
@@ -1,3 +1,7 @@
 package com.keumbang.auth.controller.dto.response;
 
-public record GetTokenResponse(String accessToken, String refreshToken) {}
+public record GetTokenResponse(String accessToken, String refreshToken) {
+  public static GetTokenResponse of(final String accessToken, final String refreshToken) {
+    return new GetTokenResponse(accessToken, refreshToken);
+  }
+}
