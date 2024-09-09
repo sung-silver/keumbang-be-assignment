e9152a39eb7d3c736d2d548a62115a6caa14dd69
sung-silver <jinna0319@gmail.com>
2024-09-09 16:01:05 +0900

    [FEAT] auth: AuthController에 로그인 로직 구현

--- keumbang-auth/src/main/java/com/keumbang/auth/controller/AuthController.java
+++ keumbang-auth/src/main/java/com/keumbang/auth/controller/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.keumbang.auth.controller.dto.request.LoginRequest;
 import com.keumbang.auth.controller.dto.request.SignUpRequest;
 import com.keumbang.auth.controller.dto.response.GetTokenResponse;
 import com.keumbang.auth.service.AuthService;
@@ -27,5 +28,10 @@ public class AuthController {
     GetTokenResponse response = authService.signUp(request);
     URI uri = URI.create("/members/" + response.memberId());
     return ResponseEntity.created(uri).body(response);
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<GetTokenResponse> login(@RequestBody @Valid final LoginRequest request) {
+    return ResponseEntity.ok(authService.login(request));
   }
 }
