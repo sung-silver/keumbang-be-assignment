ebbb52ecaf3fd60e794d2311d52232f6f40bb009
sung-silver <jinna0319@gmail.com>
2024-09-09 16:00:17 +0900

    [FEAT] auth: AuthService에 로그인(login) 로직 구현

--- keumbang-auth/src/main/java/com/keumbang/auth/service/AuthService.java
+++ keumbang-auth/src/main/java/com/keumbang/auth/service/AuthService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.keumbang.auth.common.jwt.JwtTokenService;
+import com.keumbang.auth.controller.dto.request.LoginRequest;
 import com.keumbang.auth.controller.dto.request.SignUpRequest;
 import com.keumbang.auth.controller.dto.response.GetTokenResponse;
 import com.keumbang.auth.domain.Member;
@@ -33,6 +34,15 @@ public class AuthService {
 
     memberRepository.save(member);
 
+    List<String> roles = new ArrayList<>(Arrays.asList(member.getRole().getRole()));
+    MemberAuthVO memberAuthVO = MemberAuthVO.of(member.getMemberId(), roles);
+
+    return jwtTokenService.issueToken(memberAuthVO);
+  }
+
+  public GetTokenResponse login(final LoginRequest request) {
+    Member member =
+        memberRepository.findByEmailAndPasswordOrThrow(request.email(), request.password());
     List<String> roles = new ArrayList<>(Arrays.asList(member.getRole().getRole()));
     MemberAuthVO memberAuthVO = MemberAuthVO.of(member.getMemberId(), roles);
 
