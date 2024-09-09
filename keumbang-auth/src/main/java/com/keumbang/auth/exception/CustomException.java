8f4fe2ea82e7c9a8daa37c5fd5e280b240de6eca
sung-silver <jinna0319@gmail.com>
2024-09-09 15:04:36 +0900

    [FEAT] auth: CustomException 구현

---
+++ keumbang-auth/src/main/java/com/keumbang/auth/exception/CustomException.java
@@ -0,0 +1,15 @@
+package com.keumbang.auth.exception;
+
+import com.keumbang.auth.exception.exceptionType.ExceptionType;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+  private final ExceptionType exceptionType;
+
+  public CustomException(ExceptionType exceptionType) {
+    super(exceptionType.message());
+    this.exceptionType = exceptionType;
+  }
+}
