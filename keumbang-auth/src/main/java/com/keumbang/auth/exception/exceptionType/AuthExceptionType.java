077d7d14ecd071a694b89ec195b2c2470b9de382
sung-silver <jinna0319@gmail.com>
2024-09-09 21:10:13 +0900

    [FEAT] auth: AuthException에 비밀번호 일치하지 않음에 대한 예외 타입 추가

--- keumbang-auth/src/main/java/com/keumbang/auth/exception/exceptionType/AuthExceptionType.java
+++ keumbang-auth/src/main/java/com/keumbang/auth/exception/exceptionType/AuthExceptionType.java
@@ -9,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum AuthExceptionType implements ExceptionType {
+
+  // 400 Bad Request
+  INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다"),
   // 404 Internal Server Error
   NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "해당하는 회원을 찾을 수 없습니다");
 
