2c9cd71a311680f8f4bbba3ab32aadbe6d3a32b7
sung-silver <jinna0319@gmail.com>
2024-09-09 15:54:14 +0900

    [FEAT] auth: AuthExceptionType 구현

---
+++ keumbang-auth/src/main/java/com/keumbang/auth/exception/exceptionType/AuthExceptionType.java
@@ -0,0 +1,27 @@
+package com.keumbang.auth.exception.exceptionType;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum AuthExceptionType implements ExceptionType {
+  // 404 Internal Server Error
+  NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "해당하는 회원을 찾을 수 없습니다");
+
+  private final HttpStatus status;
+  private final String message;
+
+  @Override
+  public HttpStatus status() {
+    return this.status;
+  }
+
+  @Override
+  public String message() {
+    return this.message;
+  }
+}
