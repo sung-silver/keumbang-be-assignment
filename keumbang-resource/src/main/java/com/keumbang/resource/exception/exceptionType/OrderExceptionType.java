30bc3bf6217124fafc6f14322021fec8c12d77fa
sung-silver <jinna0319@gmail.com>
2024-09-11 05:06:06 +0900

    [FIX] resource: 잘못된 주문 요청에 대한 예외 타입의 HttpStatus를 404에서 400으로 변경

--- keumbang-resource/src/main/java/com/keumbang/resource/exception/exceptionType/OrderExceptionType.java
+++ keumbang-resource/src/main/java/com/keumbang/resource/exception/exceptionType/OrderExceptionType.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum OrderExceptionType implements ExceptionType {
   // 400
-  INVALID_ORDER_TYPE(HttpStatus.NOT_FOUND, "주문 타입이 올바르지 않습니다");
+  INVALID_ORDER_TYPE(HttpStatus.BAD_REQUEST, "주문 타입이 올바르지 않습니다");
 
   private final HttpStatus status;
   private final String message;
