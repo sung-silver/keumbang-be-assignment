c8cc14d2e7e7f120fcb310b80660e549b873e625
sung-silver <jinna0319@gmail.com>
2024-09-11 03:59:56 +0900

    [FEAT] resource: 자원 서버의 모든 API에 accessToken이 필요함을 명시

--- keumbang-resource/src/main/java/com/keumbang/resource/common/config/SwaggerConfig.java
+++ keumbang-resource/src/main/java/com/keumbang/resource/common/config/SwaggerConfig.java
@@ -3,7 +3,11 @@ package com.keumbang.resource.common.config;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 
 @OpenAPIDefinition(
     info =
@@ -11,5 +15,13 @@ import io.swagger.v3.oas.annotations.info.Info;
             title = "keumbang-resource API Documentation",
             description = "keumbang-resource API Documentation",
             version = "v1"))
+@SecuritySchemes({
+  @SecurityScheme(
+      name = "Authorization",
+      type = SecuritySchemeType.APIKEY,
+      description = "access token",
+      in = SecuritySchemeIn.HEADER,
+      paramName = "Authorization"),
+})
 @Configuration
 public class SwaggerConfig {}
