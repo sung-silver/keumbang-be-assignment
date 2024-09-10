cda101306bdf0b687b9964293884148834146d3a
sung-silver <jinna0319@gmail.com>
2024-09-10 14:24:23 +0900

    [FEAT] auth: 리프레시 토큰 검증에서 발생할 수 있는 예외 타입 구현

--- keumbang-auth/src/main/java/com/keumbang/auth/exception/exceptionType/AuthExceptionType.java
+++ keumbang-auth/src/main/java/com/keumbang/auth/exception/exceptionType/AuthExceptionType.java
@@ -13,6 +13,11 @@ public enum AuthExceptionType implements ExceptionType {
   // 400 Bad Request
   DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다"),
   INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다"),
+  INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, "유효하지 않은 리프레시 토큰입니다"),
+
+  // 401 Unauthorized
+  UNAUTHORIZED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "리프레시 토큰이 만료되었습니다"),
+
   // 404 Internal Server Error
   NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "해당하는 회원을 찾을 수 없습니다");
 
