63d6cd4e03032415b736db9801ff357b51f73787
sung-silver <jinna0319@gmail.com>
2024-09-11 02:50:32 +0900

    [FEAT] auth: 공통 예외/성공 response 구현

---
+++ keumbang-auth/src/main/java/com/keumbang/auth/common/response/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.keumbang.auth.common.response;
+
+import com.keumbang.auth.exception.exceptionType.ExceptionType;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ErrorResponse {
+  private final int code;
+  private final String message;
+
+  public static ErrorResponse of(ExceptionType exceptionType) {
+    return new ErrorResponse(exceptionType.getHttpStatusCode(), exceptionType.message());
+  }
+
+  public static ErrorResponse of(ExceptionType exceptionType, String customMessage) {
+    return new ErrorResponse(exceptionType.getHttpStatusCode(), customMessage);
+  }
+}
