6cc0202f206327c3a376eee4805e4794d50a53b2
sung-silver <jinna0319@gmail.com>
2024-09-11 02:31:26 +0900

    [FEAT] resource: ExceptionType 인터페이스 구현

---
+++ keumbang-resource/src/main/java/com/keumbang/resource/exception/exceptionType/ExceptionType.java
@@ -0,0 +1,13 @@
+package com.keumbang.resource.exception.exceptionType;
+
+import org.springframework.http.HttpStatus;
+
+public interface ExceptionType {
+  HttpStatus status();
+
+  String message();
+
+  default int getHttpStatusCode() {
+    return status().value();
+  }
+}
