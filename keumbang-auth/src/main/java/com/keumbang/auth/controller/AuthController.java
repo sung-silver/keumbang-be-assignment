2850c96977e3f31f5dd10ebaf034b9afb81c0960
sung-silver <jinna0319@gmail.com>
2024-09-10 14:49:20 +0900

    [FEAT] auth: AuthController에 reissue 구현

--- keumbang-auth/src/main/java/com/keumbang/auth/controller/AuthController.java
+++ keumbang-auth/src/main/java/com/keumbang/auth/controller/AuthController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.keumbang.auth.common.jwt.JwtTokenService;
 import com.keumbang.auth.controller.dto.request.LoginRequest;
+import com.keumbang.auth.controller.dto.request.ReissueRequest;
 import com.keumbang.auth.controller.dto.request.SignUpRequest;
 import com.keumbang.auth.controller.dto.response.GetTokenResponse;
 import com.keumbang.auth.service.AuthService;
@@ -22,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
   private final AuthService authService;
+  private final JwtTokenService jwtTokenService;
 
   @PostMapping("/sign-up")
   public ResponseEntity<GetTokenResponse> signUp(@RequestBody @Valid final SignUpRequest request) {
@@ -33,5 +36,11 @@ public class AuthController {
   @PostMapping("/login")
   public ResponseEntity<GetTokenResponse> login(@RequestBody @Valid final LoginRequest request) {
     return ResponseEntity.ok(authService.login(request));
+  }
+
+  @PostMapping("/reissue")
+  public ResponseEntity<GetTokenResponse> reissue(
+      @RequestBody @Valid final ReissueRequest request) {
+    return ResponseEntity.ok(jwtTokenService.reissueToken(request));
   }
 }
