5e0eede6c45371b774e26526c14587dd465bfb72
sung-silver <jinna0319@gmail.com>
2024-09-10 16:20:45 +0900

    [FEAT] auth: BaseTimeEntity 구현

---
+++ keumbang-auth/src/main/java/com/keumbang/auth/domain/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.keumbang.auth.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseTimeEntity {
+
+  @CreatedDate public LocalDateTime createdAt;
+
+  @LastModifiedDate public LocalDateTime updatedAt;
+}
