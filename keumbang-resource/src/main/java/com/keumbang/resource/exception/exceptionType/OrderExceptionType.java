cc3ea08fc5cf73ff1834ad5cc9a11c6ff00e3b93
sung-silver <jinna0319@gmail.com>
2024-09-11 15:37:13 +0900

    [FEAT] resource: 올바르지 않은 주문 상태 변경 요청에 대한 예외 추가

--- keumbang-resource/src/main/java/com/keumbang/resource/exception/exceptionType/OrderExceptionType.java
+++ keumbang-resource/src/main/java/com/keumbang/resource/exception/exceptionType/OrderExceptionType.java
@@ -7,8 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum OrderExceptionType implements ExceptionType {
   // 400
-  INVALID_ORDER_TYPE(HttpStatus.BAD_REQUEST, "주문 타입이 올바르지 않습니다");
-
+  INVALID_ORDER_TYPE(HttpStatus.BAD_REQUEST, "주문 타입이 올바르지 않습니다"),
+  INVALID_UPDATE_ORDER(HttpStatus.BAD_REQUEST, "올바르지 않은 주문 상태 변경 요청입니다");
   private final HttpStatus status;
   private final String message;
 
