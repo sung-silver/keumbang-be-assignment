18f245b4a59dae131cf7b83c21604467b5e49b96
sung-silver <jinna0319@gmail.com>
2024-09-11 19:44:30 +0900

    [FEAT] resource: 주문 삭제에 대한 예외 추가

--- keumbang-resource/src/main/java/com/keumbang/resource/exception/exceptionType/OrderExceptionType.java
+++ keumbang-resource/src/main/java/com/keumbang/resource/exception/exceptionType/OrderExceptionType.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 public enum OrderExceptionType implements ExceptionType {
   // 400
   INVALID_ORDER_TYPE(HttpStatus.BAD_REQUEST, "주문 타입이 올바르지 않습니다"),
-  INVALID_UPDATE_ORDER(HttpStatus.BAD_REQUEST, "올바르지 않은 주문 상태 변경 요청입니다");
+  INVALID_UPDATE_ORDER(HttpStatus.BAD_REQUEST, "올바르지 않은 주문 상태 변경 요청입니다"),
+  INVALID_DELETE_ORDER(HttpStatus.BAD_REQUEST, "올바르지 않은 주문 삭제 요청입니다");
+  ;
   private final HttpStatus status;
   private final String message;
 
