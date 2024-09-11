268913d285a954c43eb69fe8d0ac0ab934239d8b
sung-silver <jinna0319@gmail.com>
2024-09-11 21:23:59 +0900

    [FEAT] resource: 인증서버로부터 인증 실패 시 사용할 예외 타입 구현

---
+++ keumbang-resource/src/main/java/com/keumbang/resource/exception/exceptionType/AuthExceptionType.java
@@ -0,0 +1,28 @@
+package com.keumbang.resource.exception.exceptionType;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum AuthExceptionType implements ExceptionType {
+
+  // 401 Unauthorized
+  AUTHORIZATION_FAILED(HttpStatus.UNAUTHORIZED, "인증에 실패하였습니다.");
+
+  private final HttpStatus status;
+  private final String message;
+
+  @Override
+  public HttpStatus status() {
+    return this.status;
+  }
+
+  @Override
+  public String message() {
+    return this.message;
+  }
+}
