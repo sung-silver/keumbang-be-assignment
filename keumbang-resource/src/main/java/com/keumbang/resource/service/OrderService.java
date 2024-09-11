b59b978b60e06839aeb10272cfe1c59ebbf43fbd
sung-silver <jinna0319@gmail.com>
2024-09-11 16:38:58 +0900

    [FEAT] resource: OrderService에 updateBuyOrderStatus 구현

--- keumbang-resource/src/main/java/com/keumbang/resource/service/OrderService.java
+++ keumbang-resource/src/main/java/com/keumbang/resource/service/OrderService.java
@@ -3,13 +3,17 @@ package com.keumbang.resource.service;
 import static com.keumbang.resource.exception.exceptionType.OrderExceptionType.*;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.keumbang.resource.controller.dto.request.CreateOrderRequest;
+import com.keumbang.resource.controller.dto.request.UpdateOrderStatusRequest;
+import com.keumbang.resource.controller.dto.response.UpdateOrderStatusResponse;
 import com.keumbang.resource.entity.Order;
 import com.keumbang.resource.entity.Product;
+import com.keumbang.resource.entity.enums.OrderStatus;
 import com.keumbang.resource.entity.enums.OrderType;
 import com.keumbang.resource.entity.enums.ProductType;
 import com.keumbang.resource.exception.CustomException;
@@ -24,6 +28,7 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
   private final OrderRepository orderRepository;
   private final ProductRepository productRepository;
+  private static final int NEXT_ORDER = 1;
 
   @Transactional
   public String createOrder(final CreateOrderRequest request) {
@@ -56,6 +61,32 @@ public class OrderService {
             || productType.equals(ProductType.PURCHASE) && orderType.equals(OrderType.BUY);
     if (isInvalidOrder) {
       throw new CustomException(INVALID_ORDER_TYPE);
+    }
+  }
+
+  public UpdateOrderStatusResponse updateBuyOrderStatus(
+      final String orderId, final UpdateOrderStatusRequest request) {
+    // TODO:: 로그인한 사용자의 ID를 가져오는 로직 구현
+    Long memberId = 1L;
+    Order order =
+        orderRepository.findByOrderIdAndCustomerIdAndOrderTypeOrThrow(
+            orderId, memberId, OrderType.BUY);
+    validateBuyOrderStatus(order.getOrderStatus(), request.orderStatus());
+    order.updateOrderStatus(request.orderStatus());
+    return UpdateOrderStatusResponse.of(order.getOrderId(), order.getOrderStatus());
+  }
+
+  private void validateBuyOrderStatus(
+      final OrderStatus currentOrderStatus, final OrderStatus requestOrderStatus) {
+    HashMap<OrderStatus, Integer> orderedStatusMap = OrderStatus.getBuyOrderStatusMap();
+    boolean isInvalidOrdering =
+        orderedStatusMap.get(currentOrderStatus) + NEXT_ORDER
+            != orderedStatusMap.get(requestOrderStatus);
+    boolean isInvalidCancelOrder =
+        currentOrderStatus.equals(OrderStatus.RECEIVED)
+            && requestOrderStatus.equals(OrderStatus.CANCELED);
+    if (isInvalidCancelOrder || isInvalidOrdering) {
+      throw new CustomException(INVALID_UPDATE_ORDER);
     }
   }
 }
