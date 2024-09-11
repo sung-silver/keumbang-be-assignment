cd7db20e4feaccc1ea7c4a92c98b5b962d19f38d
sung-silver <jinna0319@gmail.com>
2024-09-11 21:26:33 +0900

    [FEAT] resource: JwtExceptionFilter 구현

---
+++ keumbang-resource/src/main/java/com/keumbang/resource/common/security/filter/JwtExceptionFilter.java
@@ -0,0 +1,40 @@
+package com.keumbang.resource.common.security.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.keumbang.resource.common.response.ErrorResponse;
+import com.keumbang.resource.exception.CustomException;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+  private final ObjectMapper objectMapper;
+
+  @Override
+  protected void doFilterInternal(
+      HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+    try {
+      filterChain.doFilter(request, response);
+    } catch (CustomException e) {
+      response.setStatus(e.getExceptionType().getHttpStatusCode());
+      response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+      response.setCharacterEncoding("UTF-8");
+      String error = objectMapper.writeValueAsString(ErrorResponse.of(e.getExceptionType()));
+      response.getWriter().write(error);
+    }
+  }
+}
