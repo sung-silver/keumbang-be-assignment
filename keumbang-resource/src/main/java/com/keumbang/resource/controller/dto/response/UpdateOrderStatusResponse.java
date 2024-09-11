46f906a33765bf44673a14d40fa12d7c104532ed
sung-silver <jinna0319@gmail.com>
2024-09-11 15:09:42 +0900

    [FEAT] resource: UpdateOrderStatusResponse 구현

--- keumbang-resource/src/main/java/com/keumbang/resource/controller/dto/response/UpdateOrderStatusResponse.java
+++ keumbang-resource/src/main/java/com/keumbang/resource/controller/dto/response/UpdateOrderStatusResponse.java
@@ -5,7 +5,5 @@ import com.keumbang.resource.entity.enums.OrderStatus;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record UpdateOrderStatusResponse(
-	@Schema(description = "상태를 변경한 주문 ID", example = "1")
-	String orderId,
-	@Schema(description = "주문의 변경된 상태", example = "CANCELED")
-	OrderStatus currentOrderStatus) {}
+    @Schema(description = "상태를 변경한 주문 ID", example = "1") String orderId,
+    @Schema(description = "주문의 변경된 상태", example = "CANCELED") OrderStatus currentOrderStatus) {}
