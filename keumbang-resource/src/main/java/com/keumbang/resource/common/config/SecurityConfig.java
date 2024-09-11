5a996fca299383bae4e229057c007dc27af08a29
sung-silver <jinna0319@gmail.com>
2024-09-11 20:14:39 +0900

    [FEAT] resource: SecurityConfig 구현

---
+++ keumbang-resource/src/main/java/com/keumbang/resource/common/config/SecurityConfig.java
@@ -0,0 +1,93 @@
+package com.keumbang.resource.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.keumbang.resource.common.security.filter.JwtAuthenticationFilter;
+import com.keumbang.resource.common.security.filter.JwtExceptionFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+  @Value("${jwt.header.format}")
+  public static String AUTHORIZATION_FORMAT;
+
+  @Value("${jwt.access.header}")
+  public static String ACCESS_TOKEN_HEADER;
+
+  public static final String[] PERMIT_PATH = {
+    "/auth/login",
+    "/auth/sign-up",
+    "/auth/reissue",
+    "/webjars/**",
+    "/swagger-resources/**",
+    "/swagger-ui/**",
+    "/v3/api-docs/**",
+    "/webjars/**",
+    "/auth/**",
+    "/login/**",
+    "/css/**",
+    "/images/**",
+    "/js/**",
+    "/h2-console/**",
+  };
+
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  private final JwtExceptionFilter jwtExceptionFilter;
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("*")
+            .allowedOriginPatterns("*")
+            .allowedHeaders("*")
+            .allowedMethods(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.PATCH.name(),
+                HttpMethod.DELETE.name());
+      }
+    };
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable)
+        .formLogin(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable)
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .headers(
+            headerConfig ->
+                headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+        .authorizeHttpRequests(
+            auth -> auth.requestMatchers(PERMIT_PATH).permitAll().anyRequest().authenticated())
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        .addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class);
+    return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
