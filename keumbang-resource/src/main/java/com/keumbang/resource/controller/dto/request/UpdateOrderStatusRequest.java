370bd86ade7f0bde77813cbcc2baa59b297ab048
sung-silver <jinna0319@gmail.com>
2024-09-11 15:06:10 +0900

    [FEAT] resource: UpdateOrderStatusRequest 구현

---
+++ keumbang-resource/src/main/java/com/keumbang/resource/controller/dto/request/UpdateOrderStatusRequest.java
@@ -0,0 +1,16 @@
+package com.keumbang.resource.controller.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+
+import com.keumbang.resource.entity.enums.OrderStatus;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record UpdateOrderStatusRequest(
+    @NotNull(message = "주문 변경 상태는 필수 입력 값입니다.")
+        @Schema(
+            description = "변경하고 싶은 주문 상태",
+            example =
+                "BUY(구매) - DEPOSITED(입금 완료), RECEIVED(수령 완료), CANCELED(취소)"
+                    + ", SELL(판매) - TRANSFERRED(송금 완료), DELIVERED(발송 완료), CANCELED(취소)")
+        OrderStatus orderStatus) {}
