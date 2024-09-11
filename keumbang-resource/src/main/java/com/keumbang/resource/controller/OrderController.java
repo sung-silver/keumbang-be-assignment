21c9408aeb7178e7c2f57994af04ebc3819b731c
sung-silver <jinna0319@gmail.com>
2024-09-11 17:22:36 +0900

    [FEAT] OrderController에 판매 주문 및 구매 주문에 대해 주문 유형을 변경하는 API 구현

--- keumbang-resource/src/main/java/com/keumbang/resource/controller/OrderController.java
+++ keumbang-resource/src/main/java/com/keumbang/resource/controller/OrderController.java
@@ -1,10 +1,14 @@
 package com.keumbang.resource.controller;
 
+import static com.keumbang.resource.exception.exceptionType.OrderSuccessType.*;
+
 import java.net.URI;
 
 import jakarta.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.keumbang.resource.common.response.SuccessResponse;
 import com.keumbang.resource.controller.dto.request.CreateOrderRequest;
+import com.keumbang.resource.controller.dto.request.UpdateOrderStatusRequest;
+import com.keumbang.resource.controller.dto.response.UpdateOrderStatusResponse;
 import com.keumbang.resource.exception.exceptionType.OrderSuccessType;
 import com.keumbang.resource.service.OrderService;
 
@@ -32,5 +38,23 @@ public class OrderController implements OrderApi {
     URI uri = URI.create(ORDER_URL + orderId);
     return ResponseEntity.created(uri)
         .body(SuccessResponse.of(OrderSuccessType.CREATE_ORDER_SUCCESS));
+  }
+
+  @Override
+  @PatchMapping("/buy/{orderId}/status")
+  public ResponseEntity<SuccessResponse<UpdateOrderStatusResponse>> updateBuyOrder(
+      @PathVariable("orderId") final String orderId,
+      @RequestBody @Valid final UpdateOrderStatusRequest request) {
+    UpdateOrderStatusResponse response = orderService.updateBuyOrderStatus(orderId, request);
+    return ResponseEntity.ok(SuccessResponse.of(UPDATE_ORDER_STATUS_SUCCESS, response));
+  }
+
+  @Override
+  @PatchMapping("/sell/{orderId}/status")
+  public ResponseEntity<SuccessResponse<UpdateOrderStatusResponse>> updateSellOrder(
+      @PathVariable("orderId") final String orderId,
+      @RequestBody @Valid final UpdateOrderStatusRequest request) {
+    UpdateOrderStatusResponse response = orderService.updateSellOrderStatus(orderId, request);
+    return ResponseEntity.ok(SuccessResponse.of(UPDATE_ORDER_STATUS_SUCCESS, response));
   }
 }
