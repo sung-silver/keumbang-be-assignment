43acccae2ddada169478daecab075ab7b024ed24
sung-silver <jinna0319@gmail.com>
2024-09-11 15:41:03 +0900

    [FEAT] resource: OrderRepository에 주문id, 고객id, 주문 유형으로 주문을 찾는 기능 구현

--- keumbang-resource/src/main/java/com/keumbang/resource/repository/OrderRepository.java
+++ keumbang-resource/src/main/java/com/keumbang/resource/repository/OrderRepository.java
@@ -1,7 +1,23 @@
 package com.keumbang.resource.repository;
 
+import static com.keumbang.resource.exception.exceptionType.OrderExceptionType.*;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.keumbang.resource.entity.Order;
+import com.keumbang.resource.entity.enums.OrderType;
+import com.keumbang.resource.exception.CustomException;
 
-public interface OrderRepository extends JpaRepository<Order, String> {}
+public interface OrderRepository extends JpaRepository<Order, String> {
+  Order findByOrderIdAndCustomerIdAndOrderType(
+      final String orderId, final Long customerId, final OrderType orderType);
+
+  default Order findByOrderIdAndCustomerIdAndOrderTypeOrThrow(
+      final String orderId, final Long customerId, final OrderType orderType) {
+    return Optional.ofNullable(
+            findByOrderIdAndCustomerIdAndOrderType(orderId, customerId, orderType))
+        .orElseThrow(() -> new CustomException(INVALID_UPDATE_ORDER));
+  }
+}
