e3733a1461fe4b8678ee4a870b1c21725b7f3ef2
sung-silver <jinna0319@gmail.com>
2024-09-11 22:16:00 +0900

    [FEAT] resource: response, request dto에 swagger schema 정의

--- keumbang-resource/src/main/java/com/keumbang/resource/controller/dto/response/UpdateOrderStatusResponse.java
+++ keumbang-resource/src/main/java/com/keumbang/resource/controller/dto/response/UpdateOrderStatusResponse.java
@@ -4,6 +4,7 @@ import com.keumbang.resource.entity.enums.OrderStatus;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(name = "UpdateOrderStatusResponse", description = "주문 상태 변경 응답 DTO")
 public record UpdateOrderStatusResponse(
     @Schema(description = "상태를 변경한 주문 ID", example = "1") String orderId,
     @Schema(description = "주문의 변경된 상태", example = "CANCELED") OrderStatus currentOrderStatus) {
